1aae891a2f209adeb67a9feaf90150803670b9e0
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-26 09:58:27 +0300

    Add reducer index.js

--- react-car-app/src/index.js
+++ react-car-app/src/index.js
@@ -6,15 +6,28 @@ import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route, Switch }  from 'react-router-dom'
 
 
+import reducers   from './reducers';
 import './styles/index.css';
 import App from './pages/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-	<App />
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+	 <Provider store={createStoreWithMiddleware(reducers)}>
+  	<BrowserRouter>
+    	<div>
+    		<Switch>
+   				<Route path="/" component={App} />
+   			</Switch>
+    	</div>
+   </BrowserRouter>
+  </Provider>
 
  , document.getElementById('root'));
 
 
 registerServiceWorker();
+
+
